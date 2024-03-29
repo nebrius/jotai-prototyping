7bef5b4f92d5f954920781545a7bf4f6e8ae9abe
Bryan Hughes <bryan@patreon.com>
2024-03-29 11:22:42 -0700

    Upgraded to Next.js 14

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-export default function Index() {
+import styles from './app.module.css';
+
+// This needs to be an async component for Next.js to recognize this as an RSC
+// eslint-disable-next-line @typescript-eslint/require-await
+export default async function Index() {
   return (
     <div>
-      <p>
+      <div className={styles.section}>
         This sample app shows off React Strapped, Jotai, and Next.js. The demo
         is written twice: once using the older pages approach, and the other
         using the new app-router.
-      </p>
-      <p>
+      </div>
+      <div className={styles.section}>
         App-router based pages:
         <ul>
           <li>
@@ -16,18 +20,20 @@ export default function Index() {
             <a href="/app/analytics">Analytics page</a>
           </li>
         </ul>
-      </p>
-      <p>
+      </div>
+      <div className={styles.section}>
         Pages based pages:
         <ul>
           <li>
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
             <a href="/pages/settings">Settings page</a>
           </li>
           <li>
+            {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
             <a href="/pages/analytics">Analytics page</a>
           </li>
         </ul>
-      </p>
+      </div>
     </div>
   );
 }
